d4086c6b71508b1ffc7933f39ae62fa1a5c1e841
Melvin Folkers <melvin@yellowstacks.nl>
2020-10-09 16:15:41 +0200

    changes to project structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.log import set_logging
 from src.parse_settings import get_settings
+from src import buienradar
 import logging
 from datetime import datetime
 
@@ -8,10 +9,10 @@ settings = get_settings("settings/yml/run_settings.yml")
 
 def run():
 
-    logging.info("insert scripts here...")
+    actuals, forecast = buienradar.get_data()
 
 
 if __name__ == "__main__":
     set_logging()
-    logging.info(f"started script  at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    logging.info(f"started script at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     run()
